78bb00e2fe4a01c36e7f1c8bbb17d71ce43b7064
Anderson Prante <andersonprante@gmail.com>
2021-02-16 21:23:21 -0300

    Add collapse categories

--- Template/analytic/sCurve.php
+++ Template/analytic/sCurve.php
@@ -1,41 +1,63 @@
-<?php if (! $is_ajax): ?>
-    <div class="page-header">
-        <h2><?= t('s-Curve') ?></h2>
+<div class="sCurve">
+
+  <?php if (! $is_ajax): ?>
+      <div class="page-header">
+          <h2><?= t('s-Curve') ?></h2>
+      </div>
+  <?php endif ?>
+  
+  <?php if (count($payload['scurve']) <= 2): ?>
+    <p class="alert"><?= t('Not enough data to show the graph.') ?></p>
+    <?php return ?>
+  <?php endif ?>
+  
+  <?php if(count($payload['info']) > 0): ?>
+    <div class="alert">
+      <ul>
+        <?php foreach ($payload['info'] as $info): ?>
+          <li><?= $info ?></li>
+        <?php endforeach ?>
+      </ul>
     </div>
-<?php endif ?>
+  <?php endif ?>
+  
+  <?php if (empty($payload)): ?>
+    <p class="alert"><?= t('Not enough data to show the graph.') ?></p>
+    <?php else: ?>
+      <details open>
+        <summary><?= t('Full project timeline') ?></summary>
+        <div>
+          <?= $this->app->component('chart-s-curve', array(
+            'id' => 'main',
+            'payload' => $payload['scurve']
+          )) ?>
+        </div>
+      </details>
+  <?php endif ?>
+  
+  <?php if (!empty($payload['categories'])): ?>
+    <?php foreach ($payload['categories'] as $idx => $category): ?>
+      <details>
+        <summary><?= $category['name'] ?></summary>
+        <div>
+          <?= $this->app->component('chart-s-curve', array(
+          'id' => $idx,
+          'payload' => $category['tasks1']
+          )) ?>
+        </div>
+      </details>
+    <?php endforeach; ?>
+  <?php endif ?>
 
+</div>
 
-<?php if (count($payload['scurve']) <= 2): ?>
-  <p class="alert"><?= t('Not enough data to show the graph.') ?></p>
-  <?php return ?>
-<?php endif ?>
+<style>
+.sCurve details {
+  margin: 1em;
+  cursor: pointer;
+}
 
-<?php if(count($payload['info']) > 0): ?>
-  <div class="alert">
-    <ul>
-      <?php foreach ($payload['info'] as $info): ?>
-        <li><?= $info ?></li>
-      <?php endforeach ?>
-    </ul>
-  </div>
-<?php endif ?>
-
-<?php if (empty($payload)): ?>
-  <p class="alert"><?= t('Not enough data to show the graph.') ?></p>
-<?php else: ?>
-  <h2><?= t('Full project timeline') ?></h2>
-  <?= $this->app->component('chart-s-curve', array(
-    'id' => 'main',
-    'payload' => $payload['scurve']
-    )) ?>
-<?php endif ?>
-
-<?php if (!empty($payload['categories'])): ?>
-  <?php foreach ($payload['categories'] as $idx => $category): ?>
-    <h2><?= $category['name'] ?></h2>
-    <?= $this->app->component('chart-s-curve', array(
-      'id' => $idx,
-      'payload' => $category['tasks1']
-      )) ?>
-  <?php endforeach; ?>
-<?php endif ?>
+.sCurve details > div  {
+  margin: 1em;
+}
+</style>
